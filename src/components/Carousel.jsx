6b74e11d310249cfe507114737641b56dbfbba02
semperprimum <bkeem0266+github@gmail.com>
2023-08-03 17:21:27 +0600

    finished a carousel for mobile devices

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,49 +1,57 @@
-import { styled } from "styled-components";
+import slide4 from "../assets/image-slide-1.jpg";
+import slide5 from "../assets/image-slide-2.jpg";
+import slide1 from "../assets/image-slide-3.jpg";
+import slide2 from "../assets/image-slide-4.jpg";
+import slide3 from "../assets/image-slide-5.jpg";
+import { ReactComponent as IconPrev } from "../assets/icon-arrow-left.svg";
+import { ReactComponent as IconNext } from "../assets/icon-arrow-right.svg";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
 // import required modules
 import { Navigation } from "swiper/modules";
 
-const StyledSwiper = styled(Swiper)`
-  margin-block: 2rem;
-
-  & > .swiper-button-prev,
-  & > .swiper-button-next {
-    color: coral;
-  }
-`;
-const StyledSwiperSlide = styled(SwiperSlide)`
-  width: 16.875rem;
-  height: 11.25rem;
-  background-color: steelblue;
-`;
-const SwiperButton = styled(Navigation)`
-  border: none;
-  background: transparent;
-`;
+const slides = [slide1, slide2, slide3, slide4, slide5];
 
 export function Carousel() {
   return (
     <>
-      <StyledSwiper
-        slidesPerView={"auto"}
-        centeredSlides={true}
-        spaceBetween={16}
+      <Swiper
+        navigation={{
+          prevEl: ".carousel__button--prev",
+          nextEl: ".carousel__button--next",
+        }}
         loop={true}
-        navigation={true}
+        slidesPerView={"auto"}
         modules={[Navigation]}
-        className="swiper"
+        loopedSlides={2}
+        spaceBetween={32}
+        centeredSlides={true}
+        className="carousel"
       >
-        <StyledSwiperSlide>Slide 1</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 2</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 3</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 4</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 5</StyledSwiperSlide>
-        {/* <StyledSwiperSlide>Slide 6</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 7</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 8</StyledSwiperSlide>
-        <StyledSwiperSlide>Slide 9</StyledSwiperSlide> */}
-      </StyledSwiper>
+        {slides.map((slide) => (
+          <SwiperSlide
+            key={slide}
+            className="carousel__slide"
+            style={{ backgroundImage: `url("${slide}")` }}
+          />
+        ))}
+        <div className="carousel__buttons">
+          <button className="carousel__button carousel__button--prev">
+            {<IconPrev aria-hidden="true" />}
+            <p className="visually-hidden">Previous slide</p>
+          </button>
+          <button className="carousel__button carousel__button--next">
+            <IconNext aria-hidden="true" />
+            <p className="visually-hidden">Next slide</p>
+          </button>
+        </div>
+      </Swiper>
     </>
   );
 }
